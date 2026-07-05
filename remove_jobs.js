e57570d2931933003a629738d1480a0d30f6a621
nimeshrj17 <nimesh.rj17@gmail.com>
2026-07-05 22:46:07 +0530

    Remove jobs link and rename footer domain to airton.eu

---
+++ remove_jobs.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+function updateFile(filePath) {
+    let content = fs.readFileSync(filePath, 'utf8');
+    let modified = false;
+
+    // 1. Remove the <li> containing the jobs link
+    const jobsLiRegex = /<li[^>]*>[\s\S]{1,100}?<a\s+href="https:\/\/airton\.shop\/pages\/jobs"[^>]*>[\s\S]{1,100}?<\/a>[\s\S]{1,50}?<\/li>/g;
+    if (content.match(jobsLiRegex)) {
+        content = content.replace(jobsLiRegex, '');
+        modified = true;
+    }
+
+    // 2. Remove the <a> standalone if there's no <li>
+    const jobsARegex = /<a\s+href="https:\/\/airton\.shop\/pages\/jobs"[^>]*>[\s\S]{1,100}?<\/a>/g;
+    if (content.match(jobsARegex)) {
+        content = content.replace(jobsARegex, '');
+        modified = true;
+    }
+
+    if (modified) {
+        fs.writeFileSync(filePath, content, 'utf8');
+        console.log('Updated:', filePath);
+    }
+}
+
+function walkDir(dir) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const fullPath = path.join(dir, file);
+        if (fs.statSync(fullPath).isDirectory()) {
+            walkDir(fullPath);
+        } else if (fullPath.endsWith('.html')) {
+            updateFile(fullPath);
+        }
+    }
+}
+
+walkDir('./airton.shop');
+console.log('Done.');
